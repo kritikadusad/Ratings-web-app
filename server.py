274c70f9bd5c9c139762ad0b7659dbcf467f66f8
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-31 17:02:31 -0700

    Everything completed except adding comments.

--- server.py
+++ server.py
@@ -24,7 +24,10 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
 
-    return render_template("homepage.html")
+    if 'user' in session:
+        return render_template("log_in_homepage.html")
+    else:
+        return render_template("homepage.html")
 
 @app.route('/register', methods = ['GET', 'POST'])
 def register_form():
@@ -56,23 +59,25 @@ def login():
 def logged_in():
     """Logged in or not"""
 
-    username = request.form.get("user_id")
+    email = request.form.get("email")
     password = request.form.get("password")
+    user = User.query.filter(User.email == email).one()
+    user_id = int(user.user_id)
 
-    user_check = User.query.filter(User.email == username, User.password == password).all()
+    user_check = User.query.filter(User.email == email, User.password == password).all()
     
     if user_check:
-        session['user'] = username
-        print(session['user'])
+        session['user'] = email
         flash("You have successfully logged in!")
-        return redirect("/user-details")
-
+        return redirect(f"user-details/{user_id}")
+    else:
+        return redirect ("/")
 
 @app.route('/logout')
 def logout():
     """Logged out"""
 
-    session = {}
+    session.clear()
     flash("Logged out!")
     return redirect("/")
 
@@ -83,6 +88,57 @@ def user_list():
     users = User.query.all()
     return render_template("user_list.html", users=users)
 
+@app.route("/movies")
+def movie_list():
+    """Show list of movies."""
+
+    movies = Movie.query.order_by(Movie.title).all()
+    return render_template("movies.html", movies=movies)
+
+@app.route("/movie-details/<movie_id>")
+def movie_details(movie_id):
+    """ Show movie details: release date, imdb url, ratings"""
+
+    rating_obj = Rating.query.filter(Rating.movie_id == movie_id).first()
+    movie = rating_obj.movie
+    score = rating_obj.score
+    if 'user' in session:
+        return render_template("log_in_movie_details.html",
+                            movie = movie.movie_id,
+                            title = movie.title,
+                            year = movie.released_at,
+                            url = movie.imdb_url,
+                            score = score)
+    else:
+        return render_template("movie_details.html",
+                            movie = movie.movie_id,
+                            title = movie.title,
+                            year = movie.released_at,
+                            url = movie.imdb_url,
+                            score = score)
+
+@app.route('/rate-movie/<movie_id>', methods = ['POST'])
+def movie_rated(movie_id):
+    """Checks to see if movie already rated and updates the rating"""
+
+    # The below rating is from the database. We are getting it by 
+    # using the movie id and using the relationship between Movie 
+    # and Rating classes. 
+    rating = Rating.query.filter(Rating.movie_id == movie_id).first()
+    
+    user_rating = request.form.get("movie_rating")
+   
+    rating.score = user_rating
+    
+    # The rating obtained from the form on log_in_movie_details:
+
+    # Checking if a rating already exists in the database: 
+    db.session.add(rating)
+    db.session.commit()
+
+    return render_template("rating_confirmation.html")
+
+
 @app.route('/user-details/<user_id>')
 def user_details(user_id):
     """Get user details"""
@@ -90,16 +146,21 @@ def user_details(user_id):
     user_detail = Rating.query.get(user_id)
     person = user_detail.user
     person_movie = user_detail.movie
+    movie_detail = Rating.query.get(person_movie.movie_id)
+
 
     return render_template("user_details.html", 
                             age = person.age,
                             zipcode = person.zipcode,
-                            movies = person_movie.title)
+                            movies = person_movie.title, 
+                            movie_score = movie_detail.score)
 
 
 
 
 if __name__ == "__main__":
+
+
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
     app.debug = True
@@ -111,4 +172,4 @@ if __name__ == "__main__":
     # Use the DebugToolbar
     DebugToolbarExtension(app)
 
-    app.run(port=5000, host='0.0.0.0')
+    app.run(port=5000)
